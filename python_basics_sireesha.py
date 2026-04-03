b3b03651abfee22a9b80ff02277bc44562c47a7f
Sireeshaambapuram <sireeshaambapuaram@example.com>
2026-04-03 19:48:33 +0530

    Added python basics assignment

---
+++ python_basics_sireesha.py
@@ -0,0 +1,27 @@
+name = "Sireesha"  
+
+age = 21        
+
+height = 1.65      
+
+is_student = True    
+
+print(name, age, height, is_student)
+
+print(f"Name: {name} | Age: {age} | Height: {height} | Student: {is_student}")
+
+age_months = age * 12
+
+age_days = age * 365
+
+remainder = age % 7
+
+age_square = age ** 2
+
+print("Age in months:", age_months)
+
+print("Age in days:", age_days)
+
+print("Remainder when age divided by 7:", remainder)
+
+print("Age squared:", age_square)
